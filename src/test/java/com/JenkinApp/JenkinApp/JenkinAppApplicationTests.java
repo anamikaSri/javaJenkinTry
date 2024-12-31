d10e34bc9157a84a805e011255e8fd1d61fb26a1
I528604 <anamika.srivastava@sap.com>
2024-12-31 11:04:40 +0530

    Revert "Revert "Reapply "jenkin project"""
    This reverts commit 721004e1b56bb0ad004d81b64371b737ee625f1a.
    
    t

---
+++ src/test/java/com/JenkinApp/JenkinApp/JenkinAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JenkinApp.JenkinApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
